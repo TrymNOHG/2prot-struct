076e41b82b0352c64442ae25d5d33b6d2a0206d0
Nicolai Brand <nicolaihbrand@gmail.com>
2025-03-02 12:45:22 +0100

    Exploratory Analysis + baseline models (#1)

---
+++ run_model.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+df = pd.read_csv("data/data.csv")
+
+y = df['dssp8'][:1000]
+X_original = df['input'][:1000]
+X_train, X_test, y_train, y_test = train_test_split(X_original, y, test_size=0.2, random_state=42)
+
+from models.simple_window_model import TreeWindowModel
+
+model = TreeWindowModel(window_length=5)
+model.fit(X_train, y_train)
+
+print(model.evaluate(X_test, y_test))
