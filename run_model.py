7863edb369a1ca73905698b782248221e541cac1
Nicolai Brand <nicolaibrand2002@gmail.com>
2025-03-11 09:58:06 +0100

    small stuff#

--- run_model.py
+++ run_model.py
@@ -33,8 +33,8 @@ def oversample_windows(windows_X, windows_y):
 
 df = pd.read_csv("data/data.csv")
 
-y = df['dssp8'][:1_000]
-X_original = df['input'][:1_00]
+y = df['dssp8'][:2_000]
+X_original = df['input'][:2_000]
 
 model = MLPWindowModel(window_length=17)
 X, y = model.to_windows(X_original, y)
@@ -42,7 +42,7 @@ X, y = model.to_windows(X_original, y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 X_train, y_train = smote_oversample(X_train, y_train)
 
-model.fit(X_train, y_train, X_train, y_train, epochs=10)
+model.fit(X_train, y_train, X_test, y_test, epochs=20)
 y_pred = model.predict(X_test)
 
 results = eval_model.evaluate_classification(y_test, y_pred)
