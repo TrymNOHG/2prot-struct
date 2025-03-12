8d559cefd2a9298bfe00138863ba1ca3c1d67d90
Trym Hamer Gudvangen <trym.gudvangen@gmail.com>
2025-03-12 19:43:13 +0100

    feat: implement naive bayes

--- run_model.py
+++ run_model.py
@@ -2,15 +2,20 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-df = pd.read_csv("pLM/data.csv")
+df = pd.read_csv("data/data.csv")
+
 
 y = df['dssp8'][:1000]
 X_original = df['input'][:1000]
 X_train, X_test, y_train, y_test = train_test_split(X_original, y, test_size=0.2, random_state=42)
 
 from models.simple_window_model import TreeWindowModel
+from models.stochastic_model import StochasticModel
 
-model = TreeWindowModel(window_length=5)
-model.fit(X_train, y_train)
+
+# model = TreeWindowModel(window_length=17)
+model = StochasticModel()
+# model.fit(X_train, y_train)
+model.fit(y_train)
 
 print(model.evaluate(X_test, y_test))
